2380a7fda6d96276ce956a42f3f56049901c5284
rana <rana.saad.abdelhalim@fcis.asu.edu.eg>
2016-06-16 00:46:12 +0200

    added home view in protein model and root in routs

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,7 @@ Rails.application.routes.draw do
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
+  root 'protein#home'
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
